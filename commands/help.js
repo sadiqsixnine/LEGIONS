18bc590d471438fb5d83b84a7669b3a18e2dfec6
Sadiqul Rahman Rupom <153832903+sadiqsixnine@users.noreply.github.com>
2024-02-17 08:35:46 +0600

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -58,8 +58,8 @@ module.exports = {
         ) 
        .setImage('https://cdn.discordapp.com/attachments/1150827819547504741/1168917372267151370/standard.gif?ex=65538222&is=65410d22&hm=b4994392f44679da41fc9304eb69deaa3769e136057556deec0db69ae8d33a97&')
       const button1 = new ButtonBuilder()
-        .setLabel('Instagram')
-        .setURL('https://www.instagram.com/sadiq.sixnine69')
+        .setLabel('YouTube')
+        .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
         .setStyle(ButtonStyle.Link);
 
       const button2 = new ButtonBuilder()
