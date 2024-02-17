6ea377f1bda27829388efe328c53e47d34181f39
Sadiqul Rahman Rupom <153832903+sadiqsixnine@users.noreply.github.com>
2024-02-17 06:38:41 +0600

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -59,17 +59,17 @@ module.exports = {
        .setImage('https://cdn.discordapp.com/attachments/1150827819547504741/1168917372267151370/standard.gif?ex=65538222&is=65410d22&hm=b4994392f44679da41fc9304eb69deaa3769e136057556deec0db69ae8d33a97&')
       const button1 = new ButtonBuilder()
         .setLabel('YouTube')
-        .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
+        .setURL('https://www.youtube.com/@sadiqsixnine')
         .setStyle(ButtonStyle.Link);
 
       const button2 = new ButtonBuilder()
         .setLabel('Discord')
-        .setURL('https://discord.gg/FUEHs7RCqz')
+        .setURL('https://discord.gg/QAzKnnDxkx')
         .setStyle(ButtonStyle.Link);
 
       const button3 = new ButtonBuilder()
         .setLabel('Code')
-        .setURL('https://replit.com/@BEASTGAMERS1?tab=community')
+        .setURL('https://media1.tenor.com/m/RBbmXG9DmZwAAAAd/you-fool-dragon-ball.gif')
         .setStyle(ButtonStyle.Link);
 
       const row = new ActionRowBuilder()
